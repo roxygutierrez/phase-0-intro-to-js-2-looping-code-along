7075c1418c2765e097de8f36447ae97244b55cfa
RoxanneGutierrez <roxannenguyenn@gmail.com>
2022-11-07 18:03:18 -0600

    commit tree 8cd27e366a8fbd60a99595139bcae9f20b415230

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, occassion) {
+  const arrayOfMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    arrayOfMessages.push(
+      `Thank you, ${names[i]}, for the wonderful ${occassion} gift!`
+    );
+  }
+  return arrayOfMessages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
